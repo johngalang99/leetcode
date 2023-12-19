238a8e3d3a68f7ce58501e4e425f73acfa7014a4
johngalang <imman.galang@gmail.com>
2023-12-19 13:21:54 -0500

    math

---
+++ maths.py
@@ -0,0 +1,19 @@
+# print(5/2)
+
+# print(5//2)
+
+# print(-3//2)
+
+# print(int(-3/2))
+
+# print(10 % 3)
+
+# print(-10 % 2)
+import math
+
+print(math.floor(3/2))
+print(math.ceil(3/2))
+print(math.sqrt(2))
+print(math.pow(2, 3))
+
+print(math.pow(2, 200) < float('inf'))
