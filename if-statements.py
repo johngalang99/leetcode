2165633e2f75aab7af48233b6005e9d8297dc2a2
johngalang <imman.galang@gmail.com>
2023-12-19 13:06:44 -0500

    Add if-statements.py and variables.py with code changes

---
+++ if-statements.py
@@ -0,0 +1,16 @@
+n = 2
+if n > 2:
+  n -= 1
+elif n == 2:
+  n *= 2
+else:
+  n += 1
+
+print(n)
+
+
+
+n,m = 1,2
+if ((n > 2 and n != m) or n == m):
+  n += 1
+
--- variables.py
+++ variables.py
@@ -4,3 +4,12 @@ print('n =', n)
 
 n = 'abc'
 print('n =', n)
+
+
+n, m = 1, "def"
+print('n =', n)
+print('m =', m)
+
+n, m = None, None
+print('n =', n)
+print('m =', m)
