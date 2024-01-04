ad4ee83687c20312a35c559d85565a7a38ba3bbc
johngalang <imman.galang@gmail.com>
2024-01-03 21:47:43 -0500

    Add solution for minimum number of operations to make array empty

---
+++ 2870.py
@@ -0,0 +1,16 @@
+# /minimum-number-of-operations-to-make-array-empty/
+def minOperations(nums):
+    from collections import Counter
+    mp = Counter(nums)
+
+    count = 0
+    for t in mp.values():
+        if t == 1:
+            return -1
+        count += t // 3
+        if t % 3:
+            count += 1
+
+    return count
+
+
