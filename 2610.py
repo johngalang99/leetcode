53a35585d331b5eaee5612c0465b5fb36d1f8bb9
johngalang <imman.galang@gmail.com>
2024-01-05 15:16:23 -0500

    Refactor findMatrix method and add longest increasing subsequence implementation

--- 2610.py
+++ 2610.py
@@ -1,22 +1,16 @@
 # Convert an Array Into 2D Array with Conditions
 
 class Solution:
-    def findMatrix(self, v: List[int]) -> List[List[int]]:
-        um = {}
-        for i in v:
-            um[i] = um.get(i, 0) + 1
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        count = defaultdict(int)
+        res = []
 
-        ans = []
-        while um:
-            temp = []
-            to_erase = []
-            for f, s in um.items():
-                temp.append(f)
-                s -= 1
-                if s == 0:
-                    to_erase.append(f)
-                um[f] = s
-            ans.append(temp)
-            for i in to_erase:
-                del um[i]
-        return ans
+        for n in nums:
+            row = count[n]
+            if len(res) == row:
+                res.append([])
+
+            res[row].append(n)
+            count[n] += 1
+
+        return res
