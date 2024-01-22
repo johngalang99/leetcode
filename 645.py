c484ec7fb61a69aa174bb86f0e2db0c62cf86d65
johngalang <imman.galang@gmail.com>
2024-01-22 22:12:34 +0800

    Add solution for LeetCode problem: Set Mismatch

---
+++ 645.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/set-mismatch
+
+class Solution:
+    def findErrorNums(self, nums):
+        dup, missing = -1, -1
+
+        for i in range(1, len(nums) + 1):
+            count = nums.count(i)
+            if count == 2:
+                dup = i
+            elif count == 0:
+                missing = i
+
+        return [dup, missing]
+
