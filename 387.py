396808a8d438862eee01d1b5cafdf47ea7a0b41c
John <johngalang.dev@gmail.com>
2024-02-05 16:35:52 +0800

    Add solution for finding the first unique character in a string

---
+++ 387.py
@@ -0,0 +1,11 @@
+# first unique character in a string
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = Counter(s)
+
+        for i, c in enumerate(s):
+            if count[c] == 1:
+                return i
+
+        return -1
