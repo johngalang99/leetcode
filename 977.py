dfa578126981a998e845f3d401f3bf0700c55f70
John <johngalang.dev@gmail.com>
2024-03-02 20:00:02 +0800

    Add solution for sorting squares of a sorted array

---
+++ 977.py
@@ -0,0 +1,7 @@
+# squares of a sorted array
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        squares = [num * num for num in nums]
+        squares.sort()
+        return squares
